57bce83a912fd2af33a1fcae345186db1fa600af
jimmyfargo <zblizzard@live.com>
2017-02-11 20:18:41 -0600

    added sidebar

--- src/public/js/directives/directives.js
+++ src/public/js/directives/directives.js
@@ -4,4 +4,18 @@ angular.module('app.directives', [])
             restrict: 'E',         
             templateUrl: 'views/templates/user.tmp.html'
         }
+    }])
+    .directive('sidemenu', [function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/templates/sidebar.tmp.html',
+            controller: ['$scope', function($scope) {
+                $scope.toggleMenu = function() {
+                    $('.sidemenu-wrapper').toggleClass('hide-menu');
+                    $('.menu-btn').toggleClass('menu-btn-left');
+                    $('.menu-btn').toggleClass('glyphicon glyphicon-triangle-right');
+                    $('.menu-btn').toggleClass('glyphicon glyphicon-triangle-left');
+                }
+            }]
+        }
     }]);
